8dca180d671982d6b6b1a40dfc9f9ed4b473172e
CodeWith-Basit <abdul560basit@gmail.com>
2026-03-01 00:32:19 +0500

    Another file

---
+++ Question_4.dart
@@ -0,0 +1,20 @@
+void main() {
+  int number = 145;
+
+  if (number % 2 == 0) {
+    print("The number is Even!");
+  } else {
+    print("The number is Odd!");
+  }
+
+  int number_1 = 3;
+  int number_2 = 2;
+
+  if (number_1 < number_2) {
+    print("The number_1 is less than number_2");
+  } else if (number_1 == number_2) {
+    print("Both are equales");
+  } else {
+    print("The number_1 is greater than number_2");
+  }
+}
